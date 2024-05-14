7c2ba2951099509d31b3fe10bd276cda113a5bec
vby <vby@melexis.com>
2024-05-15 00:21:06 +0300

    implemented recursive subfolder search with file size and date modified acquired - TODO: check sorting by size and date

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import os
+import time
 
-size = 0
 info_dict = {}
+
 
 check_path = r'C:\Users\vby\PycharmProjects\Traverse_directories\dummy'
 
-for root, folders, files in os.walk(check_path):
-    print("-----------")
-    print(root)
-    print(folders)
-    print(files)
-    for file in files:
-        print(f"this is here {str(root+'\\'+file)}")
-        print(f"{file} is of size {os.path.getsize(str(root+'\\'+file))}")
-        size += os.path.getsize(str(root+'\\'+file))
-    info_dict[root] = size
-    size = 0
-    print(info_dict)
+def dive_deep_info(path):
+    for entry in os.scandir(path):
+        if path not in info_dict:   
+            info_dict[path] = [0,None]
+        if os.path.isfile(os.path.join(path,entry)):
+            info_dict[path][0] += os.path.getsize(os.path.join(path,entry))
+            last_modified = time.ctime(os.path.getmtime(os.path.join(path,entry)))
+            info_dict[path][1] = last_modified
+        elif os.path.isdir(os.path.join(path, entry)):
+            dive_deep_info(os.path.join(path, entry))
+            try:
+                info_dict[path][0] += info_dict[os.path.join(path, entry)][0] 
+            except KeyError:
+                continue
+
+    return info_dict
+    
+        
+#os.walk
+
+print(dive_deep_info(check_path))
+
+
